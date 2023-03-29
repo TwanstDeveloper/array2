4c99d0a312fe066779a22dd788a9d677470b5621
Twanst Codes <98418359+TwanstDeveloper@users.noreply.github.com>
2023-03-29 16:39:55 +0300

    ARRAY2

---
+++ JavaApplication5/src/javaapplication5/JavaApplication5.java
@@ -0,0 +1,15 @@
+package javaapplication5;
+
+public class JavaApplication5 {
+
+  
+    public static void main(String[] args) {
+    
+    int[] array=new int[10];
+    for(int i=0;i<10;i++){
+    array[i]=i;
+    System.out.println(array[i]);
+    }
+    }
+    
+}
